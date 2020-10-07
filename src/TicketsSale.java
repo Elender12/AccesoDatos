4943db3fd11bf9858ee9ecd91b341d9641e79435
Elen <43650610+Elender12@users.noreply.github.com>
2020-10-07 16:42:34 +0200

    Testing from main eclipse

--- src/TicketsSale.java
+++ src/TicketsSale.java
@@ -3,7 +3,7 @@ public class TicketsSale {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hey");
+		System.out.println("Testing this branch");
 	}
 
 }
